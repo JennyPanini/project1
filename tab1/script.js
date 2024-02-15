b0cc714b63b166f1a04118b1fa6f2201b8333cfb
Jennifer Chung <chunj858@newschool.edu>
2024-02-15 07:48:41 -0500

    tab2

--- tab1/script.js
+++
@@ -1,8 +0,0 @@
-var light = document.getElementById('light');
-
-document
-  .documentElement
-  .addEventListener('mousemove', function handleMouseMove(event) {
-  light.style.setProperty('--light-position-y', (event.clientY - 50) + 'px');
-  light.style.setProperty('--light-position-x', (event.clientX - 50) + 'px');
-});

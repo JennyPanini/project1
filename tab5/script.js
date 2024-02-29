c8692cb6c62a2cd1703db20ad6b40d37112cc9be
Jennifer Chung <chunj858@newschool.edu>
2024-02-29 08:14:23 -0500

    tab5

--- tab5/script.js
+++ tab5/script.js
@@ -1,30 +1,10 @@
-(function () {
+function update(e){
+      var x = e.clientX || e.touches[0].clientX
+      var y = e.clientY || e.touches[0].clientY
     
-    const link = document.querySelectorAll('nav > .container');
-    const cursor = document.querySelector('.cursor');
-  
-    const animateit = function (e) {
-          const span = this.querySelector('span');
-          const { offsetX: x, offsetY: y } = e,
-          { offsetWidth: width, offsetHeight: height } = this,
-  
-          move = 25,
-          xMove = x / width * (move * 2) - move,
-          yMove = y / height * (move * 2) - move;
-  
-          span.style.transform = `translate(${xMove}px, ${yMove}px)`;
-  
-          if (e.type === 'mouseleave') span.style.transform = '';
-    };
-  
-    const editCursor = e => {
-          const { clientX: x, clientY: y } = e;
-          cursor.style.left = x + 'px';
-          cursor.style.top = y + 'px';
-    };
-  
-    link.forEach(b => b.addEventListener('mousemove', animateit));
-    link.forEach(b => b.addEventListener('mouseleave', animateit));
-    window.addEventListener('mousemove', editCursor);
-  
-  })();
+      document.documentElement.style.setProperty('--cursorX', x + 'px')
+      document.documentElement.style.setProperty('--cursorY', y + 'px')
+    }
+    
+    document.addEventListener('mousemove',update)
+    document.addEventListener('touchmove',update)
